f1f4d96e01cf56f826a732ba0e66f28f52fcedf6
Jamal Habash <jamalhabash@Jamals-MacBook-Pro.local>
2016-05-15 10:19:52 -0400

    Basic website using template tags to display variables and play with numbers

--- collection/views.py
+++ collection/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	# this is your new view
-	return render(request, 'index.html')
+	
+	number = 6
+	number1 = 1
+	
+	return render(request, 'index.html', {
+		
+		'number': number,
+		'number1': number1,
+		
+	}
+	
+
+	)
