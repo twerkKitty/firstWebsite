0f0bfb71cfa86e35822470f4f1d1357a17c0b616
Jamal Habash <jamalhabash@Jamals-MacBook-Pro.local>
2016-05-15 09:46:39 -0400

    Basic website with links

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -16,8 +16,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from collection import views
 from django.conf.urls import include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name = 'about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 ]
